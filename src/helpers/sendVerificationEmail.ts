5db58176c770b9877f6e2d6ec73a544a22986fcf
Ankesh Kumar Thakur <thakurankeshkumar9@gmail.com>
2026-09-24 15:09:39 +0530

    Added Verified sending Domain

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -5,7 +5,7 @@ import { ApiResponse } from "@/types/ApiResponse";
 export async function sendVerificationEmail(email: string, username: string, verifyCode: string): Promise<ApiResponse> {
     try {
         await resend.emails.send({
-            from: 'onboarding@resend.dev',
+            from: `Mystry Message <noreply@${process.env.RESEND_EMAILID}>`,
             to: email,
             subject: 'Mystry Message | Verification Code',
             react: VerificationEmail({ username, otp: verifyCode }),
